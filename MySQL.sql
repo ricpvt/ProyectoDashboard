01ca5a1a2dddbded6b652426566cf577107cf409
ricpvt <158591406+ricpvt@users.noreply.github.com>
2024-05-31 09:20:25 -0700

    Update MySQL.sql

--- MySQL.sql
+++ MySQL.sql
@@ -1,6 +1,4 @@
-DROP DATABASE IF EXISTS `streamlit`;
-
-CREATE DATABASE `streamlit` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE `streamlit`;
 
 USE `streamlit`;
 
@@ -9,6 +7,6 @@ CREATE TABLE `Users` (
   `Password` varchar(32) NOT NULL,
   PRIMARY KEY (`UserId`),
   UNIQUE KEY `UserId_UNIQUE` (`UserId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
 
 INSERT INTO Users (UserId, Password) VALUES ("admin", "admin");
